894eab46b47c1c17a186bbd9984186f2b43f0196
unknown <tobidaramola77@gmail.com>
2024-01-15 09:00:53 +0100

    added metadata to improve seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
 import { UserContextProvider } from "@/context/userContext";
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Add the Google Analytics script
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-XFYKQK871V";
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+
+      gtag("js", new Date());
+      gtag("config", "G-XFYKQK871V");
+    };
+  }, []);
+
   return (
     <>
       <UserContextProvider>
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,42 @@ export default function Home() {
         <meta name="Jordan recruitments" content="Jordan recruitments" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/JR.svg" />
+
+        <meta
+          name="description"
+          content="Connecting individuals with top employers to find your ideal job and advance your career."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.jordanrecruitments.com/" />
+        <meta property="og:title" content="Jordan Recruitments" />
+        <meta
+          property="og:description"
+          content="Connecting individuals with top employers to find your ideal job and advance your career."
+        />
+        <meta
+          property="og:image"
+          content="https://www.jordanrecruitments.com/_next/image?url=%2Frecruit2.jpg&w=2048&q=75"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.jordanrecruitments.com/"
+        />
+        <meta
+          name="google-site-verification"
+          content="JROLn3xVY3fFmYJyjnO2BoICxGgAQUn5aZOeH7T40m4"
+        />
+        <meta property="twitter:title" content="Jordan Recruitments" />
+        <meta
+          property="twitter:description"
+          content="Connecting individuals with top employers to find your ideal job and advance your career."
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.jordanrecruitments.com/_next/image?url=%2Frecruit2.jpg&w=2048&q=75"
+        />
       </Head>
 
       <Navbar />
